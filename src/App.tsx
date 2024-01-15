067b2bf57b68a6abf12dac877a898c7e70f08fda
ming222555 <ming222555@yahoo.com>
2024-01-15 10:01:33 +0800

    App component add ...  QueryClientProvider, ReactQueryDevtools and ReactQueryDevtoolsProduction

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect, Suspense, lazy } from 'react';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import TransactionsPage from './pages/TransactionsPage/TransactionsPage';
 
+const queryClientForProvider = new QueryClient();
+
+const ReactQueryDevtoolsProduction = lazy(() =>
+  import('@tanstack/react-query-devtools/build/modern/production.js').then(d => ({
+    default: d.ReactQueryDevtools,
+  })),
+);
+
 function App(): JSX.Element {
-  return <TransactionsPage />;
+  const [showDevtools, setShowDevtools] = useState(false);
+
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    window.toggleDevtools = (): void => setShowDevtools(old => !old);
+  }, []);
+
+  return (
+    <QueryClientProvider client={queryClientForProvider}>
+      <TransactionsPage />
+      {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
+      {/* @ts-ignore */}
+      <button onClick={(): void => window.toggleDevtools()}>devtools</button>
+      <ReactQueryDevtools initialIsOpen={true} />
+      {showDevtools && (
+        <Suspense fallback={null}>
+          <ReactQueryDevtoolsProduction />
+        </Suspense>
+      )}
+    </QueryClientProvider>
+  );
 }
 
 export default App;

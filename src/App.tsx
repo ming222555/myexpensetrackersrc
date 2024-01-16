5fde99d4ca09161696a0ae6490bc4201aa7486bc
ming222555 <ming222555@yahoo.com>
2024-01-16 15:29:30 +0800

    Moved   <QueryClientProvider client={queryClientForProvider}>     <ReactQueryDevtools initialIsOpen={true} />       <ReactQueryDevtoolsProduction /> from App.tsx to index.tsx Removed [devtools] button

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,9 @@
-import React, { useState, useEffect, Suspense, lazy } from 'react';
-
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import React from 'react';
 
 import TransactionsPage from './pages/TransactionsPage/TransactionsPage';
 
-const queryClientForProvider = new QueryClient();
-
-const ReactQueryDevtoolsProduction = lazy(() =>
-  import('@tanstack/react-query-devtools/build/modern/production.js').then(d => ({
-    default: d.ReactQueryDevtools,
-  })),
-);
-
 function App(): JSX.Element {
-  const [showDevtools, setShowDevtools] = useState(false);
-
-  useEffect(() => {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    window.toggleDevtools = (): void => setShowDevtools(old => !old);
-  }, []);
-
-  return (
-    <QueryClientProvider client={queryClientForProvider}>
-      <TransactionsPage />
-      {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
-      {/* @ts-ignore */}
-      <button onClick={(): void => window.toggleDevtools()}>devtools</button>
-      <ReactQueryDevtools initialIsOpen={true} />
-      {showDevtools && (
-        <Suspense fallback={null}>
-          <ReactQueryDevtoolsProduction />
-        </Suspense>
-      )}
-    </QueryClientProvider>
-  );
+  return <TransactionsPage />;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import './index.scss';
 import App from './App';
 import store from './store/store';
 
+const queryClientForProvider = new QueryClient();
+
+const ReactQueryDevtoolsProduction = lazy(() =>
+  import('@tanstack/react-query-devtools/build/modern/production.js').then(d => ({
+    default: d.ReactQueryDevtools,
+  })),
+);
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+const showDevtools = true;
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <QueryClientProvider client={queryClientForProvider}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={true} />
+        {showDevtools && (
+          <Suspense fallback={null}>
+            <ReactQueryDevtoolsProduction />
+          </Suspense>
+        )}
+      </QueryClientProvider>
     </Provider>
   </React.StrictMode>,
 );

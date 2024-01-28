6fac1b373a7f34f660078ce1e581dbd89d8016ec
ming222555 <ming222555@yahoo.com>
2024-01-28 17:06:07 +0800

    .AppLayout .AppLayoutSider .AppLayoutMain

--- src/routes/AppLayout/AppLayout.tsx
+++ src/routes/AppLayout/AppLayout.tsx
@@ -5,8 +5,39 @@ import { Outlet, Link } from 'react-router-dom';
 export default function AppLayout(): JSX.Element {
   return (
     <div className='AppLayout'>
-      AppLayout Sider... <Link to='transactions'>Transactions</Link>
-      <Outlet />
+      <aside className='AppLayoutSider'>
+        <p>Sider</p>
+        <Link to='transactions'>Transactions</Link>
+        <p>Sider</p>
+        <p>Sider</p>
+        <p>Sider</p>
+        <p>Sider</p>
+        <p>Sider</p>
+        <p>Sider</p>
+        <p>Sider</p>
+        <p>Sider</p>
+        <p>Sider</p>
+        <p>Sider</p>
+        <p>Sider</p>
+        <p>Sider</p>
+        <p>Sider</p>
+        <p>Sider</p>
+        <p>Sider</p>
+        <p>Sider</p>
+        <p>Sider</p>
+        <p>Sider</p>
+        <p>Sider</p>
+        <p>Sider</p>
+        <p>Sider</p>
+        <p>Sider</p>
+        <p>Sider</p>
+        <p>Sider</p>
+        <p>Sider666</p>
+      </aside>
+      <main className='AppLayoutMain'>
+        <Outlet />
+      </main>
+      {/* AppLayout Sider... <Link to='transactions'>Transactions</Link> */}
     </div>
   );
 }
